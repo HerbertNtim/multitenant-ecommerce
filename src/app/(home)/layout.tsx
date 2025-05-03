77142cad2c4aafdd761a20371ce82e0355e26cf2
HerbertNtim <herbertntim15@gmail.com>
2025-05-03 18:48:05 +0000

    [03: Home Layout]: developed footer and changes background colors for few ui components

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 interface Props {
@@ -9,6 +10,7 @@ const HomeLayout = ({ children }: Props) => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <div className="flex-1 bg-[#f4f4f0]">{children}</div>
+      <Footer />
     </div>
   );
 };
