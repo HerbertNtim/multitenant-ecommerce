02d00975a2665edef42dd707e4ee0e7d71df3d23
HerbertNtim <herbertntim15@gmail.com>
2025-05-03 02:29:19 +0000

    [03: Home Layout]: implemented navbar logo

--- src/app/(home)/Navbar.tsx
+++ src/app/(home)/Navbar.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { cn } from "@/lib/utils"
+import { Poppins } from "next/font/google"
+import Link from "next/link"
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['700'],
+})
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='h-20 flex border-b justify-center font-medium bg-white'>
+      <Link href={'/'} className="pl-6 flex items-center">
+        <span className={cn('font-semibold text-5xl', poppins.className)}>funroad</span>
+      </Link>
+    </nav>
   )
 }
 
