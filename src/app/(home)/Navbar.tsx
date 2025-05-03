b2d616d203a3a2f7152791747d7c57418f84585f
HerbertNtim <herbertntim15@gmail.com>
2025-05-03 03:12:39 +0000

    [03: Home Layout]: developed desktop navbar

--- src/app/(home)/Navbar.tsx
+++ src/app/(home)/Navbar.tsx
@@ -1,20 +1,73 @@
-import { cn } from "@/lib/utils"
-import { Poppins } from "next/font/google"
-import Link from "next/link"
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+import { Poppins } from "next/font/google";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['700'],
-})
+  subsets: ["latin"],
+  weight: ["700"],
+});
 
-const Navbar = () => {
-  return (
-    <nav className='h-20 flex border-b justify-center font-medium bg-white'>
-      <Link href={'/'} className="pl-6 flex items-center">
-        <span className={cn('font-semibold text-5xl', poppins.className)}>funroad</span>
-      </Link>
-    </nav>
-  )
+interface NavbarItemProps {
+  href: string;
+  children: React.ReactNode;
+  isActive: boolean;
 }
 
-export default Navbar
+const NavbarItem = ({ href, children, isActive }: NavbarItemProps) => {
+  return (
+    <Button
+      variant={"outline"}
+      className={cn(
+        "bg-transparent hover:bg-transparent rounded-2xl hover:border-primary border-transparent px-3.5 text-lg", isActive && "bg-black text-white hover:bg-black hover:text-white",
+      )}
+    >
+      <Link href={href}>{children}</Link>
+    </Button>
+  );
+};
+
+const navbarItems = [
+  { href: "/", children: "Home" },
+  { href: "/about", children: "About" },
+  { href: "/features", children: "Features" },
+  { href: "/pricing", children: "Pricing" },
+  { href: "/contact", children: "Contact" },
+];
+
+const Navbar = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="h-20 flex border-b justify-between font-medium bg-white">
+      <Link href={"/"} className="pl-6 flex items-center">
+        <span className={cn("font-semibold text-5xl", poppins.className)}>
+          funroad
+        </span>
+      </Link>
+
+      <div className="hidden items-center gap-4 lg:flex">
+        {navbarItems.map((item) => (
+          <NavbarItem key={item.href} href={item.href} isActive={pathname === item.href}>
+            {item.children}
+          </NavbarItem>
+        ))}
+      </div>
+
+      <div>
+        <Button asChild variant={'secondary'} className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg">
+          <Link href={'sign-in'}>Log In</Link>
+        </Button>
+        <Button asChild className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black text-white hover:bg-pink-400 transition-colors text-lg">
+          <Link href={'sign-up'}>Start Selling</Link>
+        </Button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
