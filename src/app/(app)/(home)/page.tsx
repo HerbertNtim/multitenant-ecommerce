629212f9fc24f1a8085ce1771d63e1858af0be26
HerbertNtim <herbertntim15@gmail.com>
2025-05-04 17:04:22 +0000

    [04: Payload Integration]: fixed types generation and implemented collections for categories on payload

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,27 +1,20 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
-import { Progress } from "@/components/ui/progress";
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 
-export default function Home() {
+
+export default async function Home() {
+  const payload = await getPayload({
+    config: configPromise,
+  })
+
+  const data = await payload.find({
+    collection: 'categories',
+  })
+
+
   return (
     <div>
-      <div>
-        <Button variant={'elevated'}>I am a button</Button>
-      </div>
-      <div>
-        <Input placeholder="I am an input" />
-      </div>
-      <div>
-        <Progress value={50} />
-      </div>
-      <div>
-        <Textarea placeholder={'I am a textarea'} />
-      </div>
-      <div>
-        <Checkbox />
-      </div>
+      {JSON.stringify(data)}
     </div>
   );
 }
