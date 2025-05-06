2acf038c6cac92eda42f3c87c0be689cfb35e435
HerbertNtim <herbertntim15@gmail.com>
2025-05-06 15:29:48 +0000

    [05: Search Filters]: creating categories and formatted data incoming

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,3 +1,4 @@
+import { Category } from "@/payload-types";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import SearchFilters from "./search-filters";
@@ -17,6 +18,7 @@ const HomeLayout = async ({ children }: Props) => {
   const data = await payload.find({
     collection: 'categories',
     depth: 1,
+    pagination: false,
     where: {
       parent: {
         exists: true,
@@ -24,10 +26,19 @@ const HomeLayout = async ({ children }: Props) => {
     }
   })
 
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((subdoc) => ({
+      // Because of 'depth: 1' we are confident that 'subdoc' will be a type of "Category"
+      ...(subdoc as Category),
+      subcategories: undefined
+    }))
+  }))
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
-      <SearchFilters data={data.docs} />
+      <SearchFilters data={formattedData} />
       <div className="flex-1 bg-[#f4f4f0]">{children}</div>
       <Footer />
     </div>
