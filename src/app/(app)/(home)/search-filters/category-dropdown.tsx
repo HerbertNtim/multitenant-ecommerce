0d3f60959b8b8e063d29e8e6e3a89071540b700e
HerbertNtim <herbertntim15@gmail.com>
2025-05-07 09:09:15 +0000

    [05: Search Filters]: Implemented hook to position dropdown menu

--- src/app/(app)/(home)/search-filters/category-dropdown.tsx
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Category } from "@/payload-types";
+import { useRef, useState } from "react";
 
 interface CategoryDropdownProps {
   category: Category;
@@ -13,16 +16,40 @@ const CategoryDropdown = ({
   isActive,
   isNavigationHovered,
 }: CategoryDropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  const onMouseEnter = () => {
+    setIsOpen(true);
+  };
+  const onMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+
   return (
-    <Button
-      variant={"elevated"}
-      className={cn(
-        `h-11 rounded-2xl text-black hover:bg-${category.color} bg-transparent border-transparent hover:border-primary`,
-        isActive && !isNavigationHovered && `bg-${category.color}`
-      )}
+    <div
+      ref={dropdownRef}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      className="relative"
     >
-      {category.name}
-    </Button>
+      <div className="relative">
+        <Button
+          variant={"elevated"}
+          className={cn(
+            `h-11 rounded-2xl text-black hover:bg- bg-${category.color} border-black/50 hover:border-primary`,
+            isActive && !isNavigationHovered && `bg-${category.color}`
+          )}
+        >
+          {category.name}
+        </Button>
+        {Array.isArray(category.subcategories) && category.subcategories?.length > 0 && (
+          <div className={cn(`opacity-0 absolute -bottom-3
+            w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2`, isOpen && "opacity-100")} />
+        )}
+      </div>
+    </div>
   );
 };
 
